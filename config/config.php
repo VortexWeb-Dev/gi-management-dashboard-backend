04398f75e447d674d1d24f96350499913b7c067e
fasilofficial <muhammedfasilofficial@gmail.com>
2025-04-11 09:34:57 +0530

    Add DashboardController.php

--- config/config.php
+++ config/config.php
@@ -2,8 +2,8 @@
 
 return [
     'cache' => [
-        'enabled' => false,
-        'expiry' => 300
+        'enabled' => true,
+        'expiry' => 500
     ],
     'LISTINGS_ENTITY_TYPE_ID' => 1084,
     'SALES_DEPARTMENT_IDS' => [5, 77, 78, 442, 443]
